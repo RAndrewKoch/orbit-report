aafb7387c2e461f42f7c0233ed748f1505a0573e
gamerDraux <61572578+GamerDraux@users.noreply.github.com>
2020-03-29 18:08:23 -0500

    added bonus 1 and 2, also styled it so it looks good on the screen

---
+++ src/app/orbit-counts/orbit-counts.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Satellite } from '../satellite';
+
+@Component({
+  selector: 'app-orbit-counts',
+  templateUrl: './orbit-counts.component.html',
+  styleUrls: ['./orbit-counts.component.css']
+})
+export class OrbitCountsComponent implements OnInit {
+  @Input() satellites: Satellite[];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  countType(satellites, type){
+    let count=0;
+    for (let satellite of satellites){
+      if (satellite.type===type){
+        console.log (satellite.type, type)
+        count+=1;
+      }
+    }
+    return count;
+  }
+}
